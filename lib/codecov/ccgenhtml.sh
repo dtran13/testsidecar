506eff6c12ca0b97a8d52a3bf35d9e7df3dc88bb
dtran13 <dtran@sugarcrm.com>
2013-08-19 16:07:21 -0700

    Update ccgenhtml.sh

--- lib/codecov/ccgenhtml.sh
+++ lib/codecov/ccgenhtml.sh
@@ -2,7 +2,7 @@
 
 TEST_RESULTS="TEST_RESULTS_DIR"
 OUT="${TEST_RESULTS}/html"
-/Users/dtran/Downloads/lcov-1.9/bin/genhtml -o ${OUT} --title --function-coverage --branch-coverage --highlight ${TEST_RESULTS}/jsTestDriver.conf-coverage.dat
+/usr/local/bin/genhtml -o ${OUT} --title --function-coverage --branch-coverage --highlight ${TEST_RESULTS}/jsTestDriver.conf-coverage.dat
 
 echo '
 To view generated reports:
